0df82ad05d99d6dc7cfbc462c813c37c8edc8e0f
paul <pavel@psm7.com>
2017-07-31 15:29:58 +0300

    gulp init

---
+++ src/js/app.js
@@ -0,0 +1,2 @@
+var scene = $('#scene').get(0);
+var parallax = new Parallax(scene);
---
+++ src/js/common.js
@@ -0,0 +1,4 @@
+//= ../../node_modules/jquery/dist/jquery.js
+//= ../../node_modules/parallax-js/dist/parallax.min.js
+
+//= app.js
